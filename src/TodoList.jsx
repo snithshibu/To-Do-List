2109e63e5f1453044ec9a7e9db7db60c5b33a78a
Snith Shibu <153205112+snithshibu@users.noreply.github.com>
2023-12-27 11:00:38 +0530

    Add files via upload

---
+++ src/TodoList.jsx
@@ -0,0 +1,19 @@
+import { TodoItem } from "./TodoItem"
+
+export function TodoList({ todos, toggleTodo, deleteTodo }) {
+    return (
+        <u1 ClassName="list">
+            {todos.length === 0 && "No Todos"}
+            {todos.map(todo => {
+                return (
+                    <TodoItem 
+                        {...todo} 
+                        key={todo.id} 
+                        toggleTodo={toggleTodo} 
+                        deleteTodo={deleteTodo} 
+                    />
+                )
+            })}
+        </u1>
+    )
+}
